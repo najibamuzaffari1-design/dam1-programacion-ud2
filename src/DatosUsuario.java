43636b4f3ec4bc0b7b8ced198ddfbc46c9dd4800
najibamuzaffari1-design <najibamuzaffari1@gmail.com>
2025-10-03 13:42:17 +0200

    Create DatosUsuario.java

---
+++ src/DatosUsuario.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class DatosUsuario {
+public static void main(String[] args) {
+    Scanner scanner = scanner.nextLine();
+    System.out.println(".()");
+    System.out.println(String)
+
+
+
+
+}
